600c65909307117b0e2fbfd538eb2ca864e14f57
Alex Ott <alexey.ott@databricks.com>
2020-10-21 13:40:43 +0200

    first test, found a bug

--- src/test/scala/com/overwatch/labs/overwatch/utils/ToolsTest.scala
+++ src/test/scala/com/overwatch/labs/overwatch/utils/ToolsTest.scala
@@ -14,11 +14,26 @@ class ToolSpec extends AnyFunSpec with SparkSessionTestWrapper {
         |    {"name":"Jai", "email":"jai87@gmail.com"}
         |]}  """.stripMargin
 
-    it("should convert string to JSON") {
+    it("should convert JSON string to Map") {
       val jsonMap = JsonUtils.jsonToMap(jsonStr)
       val bookArray = jsonMap.get("employees")
       assert(bookArray.isDefined && bookArray.get.isInstanceOf[List[Any]])
     }
+
+    it("should convert object to JSON string without nulls & empty values") {
+      val map = Map[String, Any]("sfield1" -> "value", "sfield2" -> "",
+        "sfield3" -> null, "ifield" -> 0, "bfield" -> true)
+      val js = JsonUtils.objToJson(map)
+      assertResult(map)(js.fromObj)
+// TODO: implement when the implementation is fixed
+//      println(s"${js.compactString}")
+//      println(s"${js.escapedString}")
+//      println(s"${js.prettyString}")
+//      assertResult("")(js.compactString)
+//      assertResult("")(js.escapedString)
+//      assertResult("")(js.prettyString)
+    }
+
   }
 
 }
