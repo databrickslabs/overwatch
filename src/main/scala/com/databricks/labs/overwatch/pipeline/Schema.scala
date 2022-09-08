4eb9abef8431d421821b8fedda3012ae42024358
Daniel Tomes <10840635+GeekSheikh@users.noreply.github.com>
2022-09-08 17:29:16 -0400

    schema cleanup

--- src/main/scala/com/databricks/labs/overwatch/pipeline/Schema.scala
+++ src/main/scala/com/databricks/labs/overwatch/pipeline/Schema.scala
@@ -504,11 +504,6 @@ object Schema extends SparkSessionWrapper {
     StructField("workspace_name",StringType,nullable = true)
   ))
 
-  val tagsSchema = StructType(Seq(
-    StructField("default_tags", MapType(StringType,StringType,valueContainsNull = true),nullable = true),
-    StructField("custom_tags", MapType(StringType,StringType,valueContainsNull = true),nullable = true)
-  ))
-
   /**
    * Minimum required schema by module. "Minimum Requierd Schema" means that at least these columns of these types
    * must exist for the downstream ETLs to function.
@@ -653,11 +648,12 @@ object Schema extends SparkSessionWrapper {
       StructField("min_idle_instances",LongType, nullable = true),
       StructField("max_capacity",LongType, nullable = true),
       StructField("preloaded_spark_versions",StringType, nullable = true),
+      StructField("aws_attributes",MapType(StringType,StringType, valueContainsNull = true), nullable = true),
       StructField("azure_attributes",MapType(StringType,StringType, valueContainsNull = true), nullable = true),
+      StructField("custom_tags",MapType(StringType,StringType, valueContainsNull = true), nullable = true),
       StructField("create_details", poolsCreateSchema, nullable = true),
       StructField("delete_details", poolsDeleteSchema, nullable = true),
-      StructField("request_details", poolsRequestDetails, nullable = true),
-      StructField("tags",tagsSchema,nullable = true)
+      StructField("request_details", poolsRequestDetails, nullable = true)
     )),
     // sparkJob
     3010 -> StructType(Seq(
