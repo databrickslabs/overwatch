2a91aea4ab986532ebb02a955b3cc3d70519c0f3
sriram-databricks <sriram.mohanty@databricks.com>
2022-09-12 22:46:08 +0530

    retrywrite and externalise api

--- src/test/scala/com/databricks/labs/overwatch/ParamDeserializerTest.scala
+++ src/test/scala/com/databricks/labs/overwatch/ParamDeserializerTest.scala
@@ -49,7 +49,11 @@ class ParamDeserializerTest extends AnyFunSpec {
         None,
         IntelligentScaling(),
         Some("myTestWorkspace"),
-        false
+        false,
+        None,
+        None,
+        None,
+        ""
       )
       assertResult(expected)(mapper.readValue[OverwatchParams](incomplete))
 
