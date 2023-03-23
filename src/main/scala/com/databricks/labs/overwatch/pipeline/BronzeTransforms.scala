c83bbbc8d971ca7e6763e9395adc044900d4fe85
Aman <aman.jain@databricks.com>
2023-03-23 19:28:47 +0530

    code to handle catalog reset

--- src/main/scala/com/databricks/labs/overwatch/pipeline/BronzeTransforms.scala
+++ src/main/scala/com/databricks/labs/overwatch/pipeline/BronzeTransforms.scala
@@ -737,7 +737,6 @@ trait BronzeTransforms extends SparkSessionWrapper {
           // Enable Spark to read case sensitive columns
           spark.conf.set("spark.sql.caseSensitive", "true")
 
-          println(s"baseEventsDF with try badRecordsPath----------${badRecordsPath}")
           // read the df and convert the timestamp column
           val baseDF = spark.read.option("badRecordsPath", badRecordsPath)
             .json(pathsGlob: _*)
