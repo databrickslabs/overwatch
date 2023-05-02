1b6457dfc2d06f61cec500ca3844014bf9ff3e5e
Sourav Banerjee <30810740+Sourav692@users.noreply.github.com>
2023-05-02 11:23:30 +0530

    Snapshot Incremental

--- src/main/scala/com/databricks/labs/overwatch/pipeline/Migration.scala
+++ src/main/scala/com/databricks/labs/overwatch/pipeline/Migration.scala
@@ -3,20 +3,15 @@ package com.databricks.labs.overwatch.pipeline
 import com.databricks.labs.overwatch.env.{Database, Workspace}
 import com.databricks.labs.overwatch.utils._
 import org.apache.log4j.{Level, Logger}
-import org.apache.spark.sql.streaming.Trigger
-
-import scala.collection.parallel.ForkJoinTaskSupport
-import scala.concurrent.forkjoin.ForkJoinPool
 import com.databricks.labs.overwatch.utils.Helpers.removeTrailingSlashes
-import io.delta.tables.DeltaTable
-import org.apache.spark.sql.{Column, DataFrame, DataFrameWriter, Row}
 
-class Migration (_sourceETLDB: String, _targetPrefix: String, _workspace: Workspace, _database: Database, _config: Config)
+
+class Migration(_sourceETLDB: String, _targetPrefix: String, _workspace: Workspace, _database: Database, _config: Config)
   extends Pipeline(_workspace, _database, _config){
 
 
   import spark.implicits._
-  private val snapshotRootPath = removeTrailingSlashes(_targetPrefix)
+  private val migrateRootPath = removeTrailingSlashes(_targetPrefix)
   private val workSpace = _workspace
   private val targetPrefix = _targetPrefix
   private val sourceETLDB = _sourceETLDB
@@ -32,13 +27,13 @@ class Migration (_sourceETLDB: String, _targetPrefix: String, _workspace: Worksp
                                           sourceToSnap: Array[PipelineTable]
                                         ): Seq[CloneDetail] = {
 
-    val finalSnapshotRootPath = s"${snapshotRootPath}/data"
+    val finalMigrateRootPath= s"${migrateRootPath}/data"
     val cloneSpecs = sourceToSnap.map(dataset => {
       val sourceName = dataset.name.toLowerCase
       val sourcePath = dataset.tableLocation
       val mode = dataset._mode
       val immutableColumns = (dataset.keys ++ dataset.incrementalColumns).distinct
-      val targetPath = s"$finalSnapshotRootPath/$sourceName"
+      val targetPath = s"$finalMigrateRootPath/$sourceName"
       CloneDetail(sourcePath, targetPath, None, "Deep",immutableColumns,mode)
     }).toArray.toSeq
     cloneSpecs
@@ -79,7 +74,7 @@ class Migration (_sourceETLDB: String, _targetPrefix: String, _workspace: Worksp
 
     val cloneSpecs = buildCloneSpecs(sourceToSnapFiltered) :+ CloneDetail(sourceConfigPath, targetConfigPath, None, cloneLevel)
     val cloneReport = Helpers.parClone(cloneSpecs)
-    val cloneReportPath = s"${snapshotRootPath}/clone_report/"
+    val cloneReportPath = s"${migrateRootPath}/clone_report/"
     cloneReport.toDS.write.format("delta").save(cloneReportPath)
     this
   }
@@ -110,7 +105,6 @@ object Migrate extends SparkSessionWrapper {
             sourceETLDB : String,
             targetPrefix : String,
             pipeline : String,
-            snapshotType: String,
             excludes: Option[String],
             CloneLevel: String,
             sourceConfigPath : String,
@@ -129,7 +123,7 @@ object Migrate extends SparkSessionWrapper {
    * Create a backup of the Overwatch datasets
    *
    * @param arg(0)        Source Database Name.
-   * @param arg(1)        Target snapshotRootPath
+   * @param arg(1)        Target MigrateRootPath
    * @param arg(2)        Define the Medallion Layers. Argumnent should be in form of "Bronze, Silver, Gold"(All 3 or any combination of them)
    * @param arg(3)        Type of Snapshot to be performed. Full for Full Snapshot , Incremental for Incremental Snapshot
    * @param arg(4)        Array of table names to exclude from the snapshot
@@ -140,25 +134,24 @@ object Migrate extends SparkSessionWrapper {
   def main(args: Array[String]): Unit = {
 
     val sourceETLDB = args(0)
-    val snapshotRootPath = args(1)
+    val MigrateRootPath = args(1)
     val pipeline = args(2)
-    val snapshotType = args(3)
-    val sourceConfigPath = args(4)
-    val targetConfigPath = args(5)
-    val targetETLDB = args(6)
-    val targetConsumerDB = args(7)
-    val tablesToExclude = args.lift(8).getOrElse("")
-    val cloneLevel = args.lift(9).getOrElse("Deep")
+    val sourceConfigPath = args(3)
+    val targetConfigPath = args(4)
+    val targetETLDB = args(5)
+    val targetConsumerDB = args(6)
+    val tablesToExclude = args.lift(7).getOrElse("")
+    val cloneLevel = args.lift(8).getOrElse("Deep")
 
     val snapWorkSpace = Helpers.getWorkspaceByDatabase(sourceETLDB)
 
     val pipelineLower = pipeline.toLowerCase
-    if (pipelineLower.contains("bronze")) Migrate(snapWorkSpace,sourceETLDB,snapshotRootPath,"Bronze",snapshotType,Some(tablesToExclude),cloneLevel,sourceConfigPath,targetConfigPath,targetETLDB,targetConsumerDB)
-    if (pipelineLower.contains("silver")) Migrate(snapWorkSpace,sourceETLDB,snapshotRootPath,"Silver",snapshotType,Some(tablesToExclude),cloneLevel,sourceConfigPath,targetConfigPath,targetETLDB,targetConsumerDB)
-    if (pipelineLower.contains("gold")) Migrate(snapWorkSpace,sourceETLDB,snapshotRootPath,"Gold",snapshotType,Some(tablesToExclude),cloneLevel,sourceConfigPath,targetConfigPath,targetETLDB,targetConsumerDB)
-    Migrate(snapWorkSpace,sourceETLDB,snapshotRootPath,"pipeline_report",snapshotType,Some(tablesToExclude),cloneLevel,sourceConfigPath,targetConfigPath,targetETLDB,targetConsumerDB)
+    if (pipelineLower.contains("bronze")) Migrate(snapWorkSpace,sourceETLDB,MigrateRootPath,"Bronze",Some(tablesToExclude),cloneLevel,sourceConfigPath,targetConfigPath,targetETLDB,targetConsumerDB)
+    if (pipelineLower.contains("silver")) Migrate(snapWorkSpace,sourceETLDB,MigrateRootPath,"Silver",Some(tablesToExclude),cloneLevel,sourceConfigPath,targetConfigPath,targetETLDB,targetConsumerDB)
+    if (pipelineLower.contains("gold")) Migrate(snapWorkSpace,sourceETLDB,MigrateRootPath,"Gold",Some(tablesToExclude),cloneLevel,sourceConfigPath,targetConfigPath,targetETLDB,targetConsumerDB)
+    Migrate(snapWorkSpace,sourceETLDB,MigrateRootPath,"pipeline_report",Some(tablesToExclude),cloneLevel,sourceConfigPath,targetConfigPath,targetETLDB,targetConsumerDB)
 
-    println("SnapShot Completed")
+    println("Migration Completed")
   }
 
 
