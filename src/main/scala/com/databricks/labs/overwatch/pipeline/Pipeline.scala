b1074e35316d572e124286ae38723d27b10ba75f
sriram251-code <sriram.mohanty@databricks.com>
2023-09-22 19:17:51 +0530

    code modified

--- src/main/scala/com/databricks/labs/overwatch/pipeline/Pipeline.scala
+++ src/main/scala/com/databricks/labs/overwatch/pipeline/Pipeline.scala
@@ -387,8 +387,8 @@ class Pipeline(
     // cleanse the temp dir
     // if failure doesn't allow pipeline to get here, temp dir will be cleansed on workspace init
     if (!config.externalizeOptimize) postProcessor.optimize(this, Pipeline.OPTIMIZESCALINGCOEF)
-    //Helpers.fastrm(Array(config.tempWorkingDir))
-    //dbutils.fs.rm(config.tempWorkingDir)
+    Helpers.fastrm(Array(config.tempWorkingDir))
+    dbutils.fs.rm(config.tempWorkingDir)
 
     postProcessor.refreshPipReportView(pipelineStateViewTarget)
     //TODO clearcache will clear global cache multithread performance issue
