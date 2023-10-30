b988c582456cac4dc7ce6a7f1338a72fb2410a97
Sourav Banerjee <30810740+Sourav692@users.noreply.github.com>
2023-10-31 00:28:18 +0530

    26-Oct-31: Change Lag Day in AuditLogBronze to 1 day

--- src/main/scala/com/databricks/labs/overwatch/pipeline/Gold.scala
+++ src/main/scala/com/databricks/labs/overwatch/pipeline/Gold.scala
@@ -310,7 +310,7 @@ class Gold(_workspace: Workspace, _database: Database, _config: Config)
   lazy private val appendNotebookCommandsFactProcess: () => ETLDefinition = {
     () =>
       ETLDefinition(
-        BronzeTargets.auditLogsTarget.asIncrementalDF(notebookCommandsFactModule, BronzeTargets.auditLogsTarget.incrementalColumns,30),
+        BronzeTargets.auditLogsTarget.asIncrementalDF(notebookCommandsFactModule, BronzeTargets.auditLogsTarget.incrementalColumns,1),
         Seq(buildNotebookCommandsFact(
           GoldTargets.notebookTarget,
           GoldTargets.clusterStateFactTarget
